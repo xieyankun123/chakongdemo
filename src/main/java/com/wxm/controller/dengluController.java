56535e951f78fe78a0b3d2094623e4adc1642bf6
xieyankun <1241030208@qq.com>
2018-12-12 18:39:01 +0800

    123

--- src/main/java/com/wxm/controller/dengluController.java
+++ src/main/java/com/wxm/controller/dengluController.java
@@ -14,7 +14,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+//lianxifenzhi
 @Controller
 @RequestMapping("/login.req")
 public class dengluController {
